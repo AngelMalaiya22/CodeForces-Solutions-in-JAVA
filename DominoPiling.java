e894977e4b8888f7ed06ae2143ac12e7b564efb8
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-20 03:22:24 +0530

    Add files via upload

---
+++ DominoPiling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class DominoPiling
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int M=sc.nextInt();
+        int N=sc.nextInt();
+        int ans=(M*N)/2;
+        System.out.println(ans);
+    }
+}
