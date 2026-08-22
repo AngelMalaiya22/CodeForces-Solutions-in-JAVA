294046235012fe24e72a03dc4130a4385e42b336
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-22 12:57:42 +0530

    Add files via upload

---
+++ PetyaandStrings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class PetyaandStrings
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        if(s1.equals(s2))
+        {
+            System.out.println("0");
+        }
+        else
+        {
+            int result=s1.compareTo(s2);
+            if(result<0)
+            {
+                System.out.println("-1");
+            }
+            else
+            {
+                System.out.println("1");
+            }
+        }
+    }
+}
