ae797fe3326b6948775105e09591c3db50e77b14
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-24 19:53:55 +0530

    Add files via upload

---
+++ WrongSubtraction.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class WrongSubtraction
+{
+    public static void main(String args[])
+    {
+        int d=0;
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int k= sc.nextInt();
+        while(k>0)
+        {
+            if(n%10 == 0)
+            {
+                n/=10;
+            }
+            else
+            {
+                n-=1;
+            }
+            k--;
+        }
+        System.out.println(n);
+    }
+}
