652ed2995989ad986aa5841949467a774ee6db6a
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-25 20:21:07 +0530

    Add files via upload

---
+++ Nearly_Lucky_Number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Nearly_Lucky_Number
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner (System.in);
+        String s = sc.next();
+        
+        int luckyCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '4' || c == '7') {
+                luckyCount++;
+            }
+        }
+        
+        if (luckyCount == 4 || luckyCount == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
